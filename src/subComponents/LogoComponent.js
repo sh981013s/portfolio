76bfa43cc836b50bb04a1fea9f4aecebd171789a
SeungHwan-Lee <sh981013s@gmail.com>
2021-12-14 01:21:19 +0900

    basic animations

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,23 +1,30 @@
 import styled from "styled-components";
 import {DarkTheme} from "../components/Themes";
+import {Link} from "react-router-dom";
 
 const Logo = styled.div`
 display: inline-block;
-color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
   font-family: 'Pacifico', cursive;
   font-size: calc(1rem + 1.2vh);
-  
   position: fixed;
   left: 2rem;
   top: 2rem;
   z-index: 3;
+  .title {
+    color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
+    text-decoration: none;
+  }
 `
 
 const LogoComponent = (props) => {
     return (
-        <Logo color={props.theme}>
-            SeungHwan
-        </Logo>
+            // <Link to='/'>
+                <Logo color={props.theme}>
+                    <Link to='/' className='title'>
+                        SeungHwan
+                    </Link>
+                </Logo>
+            // </Link>
     )
 }
 
